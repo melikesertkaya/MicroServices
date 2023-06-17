ffe0fd69e734126c3d22e129dda49cbb8d8ba237
melike sertkaya <melikesertkaya99@gmail.com>
2023-06-17 17:21:52 +0100

    Added methods for Course

---
+++ Services/Catalog/Course.Services.Catalog/Services/ICourseService.cs
@@ -0,0 +1,17 @@
+﻿using Course.Services.Catalog.Dtos;
+using Course.Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Course.Services.Catalog.Services
+{
+    interface ICourseService
+    {
+        Task<Response<List<CourseDto>>> GetAllAsync();
+        Task<Response<CourseDto>> GetByIdAsync(string id);
+        Task<Response<List<CourseDto>>> GetAllByUserIdAsync(string userId);
+        Task<Response<CourseDto>> CreateAsync(CourseCreatedDto courseCreateDto);
+        Task<Response<NoContent>> UpdateAsync(CourseUpdatedDto courseUpdateDto);
+        Task<Response<NoContent>> DeleteAsync(string id);
+    }
+}
