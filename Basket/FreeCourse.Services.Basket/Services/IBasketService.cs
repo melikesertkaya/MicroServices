e15485b6299d7f1eb5205ae029e2daa1b75d08e1
melike sertkaya <melikesertkaya99@gmail.com>
2023-10-06 14:39:26 +0100

    Basket Api created

---
+++ Basket/FreeCourse.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,13 @@
+﻿using Course.Shared.Dtos;
+using FreeCourse.Services.Basket.Dtos;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+        Task<Response<bool>> SaveOrUpdate(BasketDto basket);
+        Task<Response<bool>> Delete(string userId);
+    }
+}
