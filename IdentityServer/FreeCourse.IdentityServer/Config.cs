7ada9410affabd3fcbddd4c1e5f52186b3b5289a
melike sertkaya <melikesertkaya99@gmail.com>
2023-10-11 14:12:36 +0100

    Discount api created

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -16,6 +16,7 @@ namespace FreeCourse.IdentityServer
                new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
                new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
                new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
+               new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
                new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
         public static IEnumerable<IdentityResource> IdentityResources =>
@@ -33,6 +34,7 @@ namespace FreeCourse.IdentityServer
                 new ApiScope("catalog_fullpermission"," full permission for Catalog API"),
                 new ApiScope("photo_stock_fullpermission"," full permission for Photo Stock API"),
                 new ApiScope("basket_fullpermission"," full permission for Basket API"),
+                new ApiScope("discount_fullpermission"," full permission for Discount API"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -56,7 +58,7 @@ namespace FreeCourse.IdentityServer
                     AllowOfflineAccess=true,
                     ClientSecrets= {new Secret("secret".Sha256())},
                     AllowedGrantTypes= GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes={ "basket_fullpermission",IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,
+                    AllowedScopes={ "basket_fullpermission","discount_fullpermission",IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,
                     IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.OfflineAccess,"roles",IdentityServerConstants.LocalApi.ScopeName },
                     AccessTokenLifetime=1*60*60,
                     RefreshTokenExpiration=TokenExpiration.Absolute,
