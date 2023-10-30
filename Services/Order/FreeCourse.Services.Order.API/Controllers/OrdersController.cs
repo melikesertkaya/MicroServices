f7062d1ed199167b1bc78c1bf98569054db620d6
melike sertkaya <melikesertkaya99@gmail.com>
2023-10-30 15:06:51 +0000

    Orders Controller created

--- Services/Order/FreeCourse.Services.Order.API/Controllers/OrdersController.cs
+++ Services/Order/FreeCourse.Services.Order.API/Controllers/OrdersController.cs
@@ -1,13 +1,37 @@
-﻿using MediatR;
+﻿using FreeCourse.Services.Order.Application.Command;
+using FreeCourse.Services.Order.Application.Queries;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Services;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Order.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class OrdersController : ControllerBase
+    public class OrdersController : CustomBaseController
     {
-   
+        private readonly IMediator _mediator;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public OrdersController(IMediator mediator, ISharedIdentityService sharedIdentityService)
+        {
+            _mediator = mediator;
+            _sharedIdentityService = sharedIdentityService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetOrder()
+        {
+            var response=await _mediator.Send(new GetOrdersByUserIdQuery { UserId=_sharedIdentityService.GetUserId});
+            return CreateActionResultInstance(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult>SaveOrder(CreateOrderCommand createOrderCommand)
+        {
+            var response = await _mediator.Send(createOrderCommand);
+            return CreateActionResultInstance(response);
+        }
     }
 }
